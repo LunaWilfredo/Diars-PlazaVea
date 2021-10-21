0560f1b7fb75e5ba2064220f4ea87329f7c6492d
Wilfredo Luna <61670708+LunaWilfredo@users.noreply.github.com>
2021-10-20 21:13:12 -0500

    buscar producto

--- View/Content/buscarProducto.php
+++ View/Content/buscarProducto.php
@@ -9,141 +9,140 @@
                     </div>
                 </div>
                 <!-- Body content -->
-                <div class="col-lg-3">
-                    <!-- Title -->
-                    <p class="text-center my-2">Buscar producto</p>
+                <div class="col-lg-12 border px-2 pt-2 text-center">
                     <!-- Registrar Productos -->
                     <form action="" method="post">
-                        <div class="form-row">
-                            <div class="col-md-5">
-                                <label for="codigoproducto">Lote de Producto</label>
-                                <input type="text" class="form-control text-center" name="" value="0001" readonly>
-                            </div>
-                            <div class="col-md-12">
-                                <label for="codigoproducto">Descripcion de Producto</label>
-                                <input type="text" class="form-control text-center" name="" value="Arroz Costeño">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="codigoproducto">Cantidad Retiro</label>
-                                <input type="text" class="form-control text-center" name="" value="4">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="codigoproducto">Categoria</label>
-                                <input type="text" class="form-control text-center" name="" value="Categoria" readonly>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="codigoproducto">Fecha Retiro</label>
-                                <input type="text" class="form-control text-center" name="" value="25/09/2021" readonly>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="codigoproducto">Fecha Vencimiento (P)</label>
-                                <input type="text" class="form-control text-center" name="" value="25/09/2021" readonly>
+                        <div class="form">
+                            <div class="row">
+                                <div class="col-md-2 py-2">
+                                    <label for="codigoproducto">Descripcion de Producto</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input type="text" class="form-control text-center w-100" name="" value="" placeholder="Ingresar codigo de Producto">
+                                </div>
+                                <div class="col-md-">
+                                    <button type="button" class="btn btn-success">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        <div class="text-center my-2">
-                            <button type="button" class="btn btn-success " ">
-                                <i class="fas fa-search"></i> Buscar Productos
-                            </button>
-                            <button type="button" class="btn btn-warning " ">
-                                <i class="far fa-plus-square"></i> Registrar Productos
-                            </button>
-                        </div>
-                    </form>
-                    <div class="border p-2">
-                        <h6 class="font-weight-bold">Notes:</h6>
-                        <p class="lead">Buscar Producto e Ingresar Cantidad a Retirar</p>
-                        <p class="lead">(P) Producto</p>
-                    </div>
+                    </form> 
+                </div>
+                <div class="col-lg-12 ">
+                    <p class="font-weight-bold text-center pt-2">Lista de Productos Tienda</p>
                     
-                </div>
+                        <table class=" table text-center px-5" id="dataTable" width="100%" cellspacing="0">
+                            <thead class="text-center bg-danger text-light">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Codigo</th>
+                                    <th>Descripcion</th>
+                                    <th>Marca</th>
+                                    <th>Precio Unitario.(s/.)</th>
+                                    <th>Cantidad</th>
+                                    <th>Categoria</th>
+                                    <th>Fecha Retiro</th>
+                                    <th>Fecha Vencimiento (P)</th>
+                                    <th>Ubicacion</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>001</td>
+                                    <td>Producto Seleccionado</td>
+                                    <td></td>
+                                    <td>12.00</td>
+                                    <td>2</td>
+                                    <td>aceites</td>
+                                    <td>25/09/2021</td>
+                                    <td>25/09/2021</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#ubicacionModal">
+                                            <i class="fas fa-eye"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>002</td>
+                                    <td>Producto Seleccionado</td>
+                                    <td></td>
+                                    <td>12.00</td>
+                                    <td>2</td>
+                                    <td>aceites</td>
+                                    <td>25/09/2021</td>
+                                    <td>25/09/2021</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#ubicacionModal">
+                                            <i class="fas fa-eye"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>003</td>
+                                    <td>Producto Seleccionado</td>
+                                    <td></td>
+                                    <td>12.00</td>
+                                    <td>2</td>
+                                    <td>aceites</td>
+                                    <td>25/09/2021</td>
+                                    <td>25/09/2021</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#ubicacionModal">
+                                            <i class="fas fa-eye"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>004</td>
+                                    <td>Producto Seleccionado</td>
+                                    <td></td>
+                                    <td>12.00</td>
+                                    <td>2</td>
+                                    <td>aceites</td>
+                                    <td>25/09/2021</td>
+                                    <td>25/09/2021</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#ubicacionModal">
+                                            <i class="fas fa-eye"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
 
-                <div class="col-lg border-left-danger text-center">
-                        <p class="font-weight-bold">Lista de Productos Tienda</p>
-                        <div class="container-fluid">
-                            <table class="table-responsive table" id="dataTable" width="100%" cellspacing="0">
-                                <thead class="text-center bg-danger text-light">
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Descripcion</th>
-                                        <th>Precio Unitario.(s/.)</th>
-                                        <th>Cantidad</th>
-                                        <th>Categoria</th>
-                                        <th>Fecha Retiro</th>
-                                        <th>Fecha Vencimiento (P)</th>
-                                        <th>Accion</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><a href="" class="text-dark text-decoration-none">1</a></td>
-                                        <td>Producto Seleccionado</td>
-                                        <td>12.00</td>
-                                        <td>
-                                            2
-                                            <div class="form-row">
-                                                <div class="col-12">
-                                                    <input type="text" class="form-control text-center" name="" value="2">
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>aceites</td>
-                                        <td>25/09/2021</td>
-                                        <td>25/09/2021</td>
-                                        <td>
-                                            <a href="" class="btn btn-danger"><i class="far fa-minus-square"></i></a>
-                                            <a href="" class="btn btn-primary"><i class="far fa-edit"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="" class="text-dark text-decoration-none">2</a></td>
-                                        <td>Producto Seleccionado</td>
-                                        <td>12.00</td>
-                                        <td>2</td>
-                                        <td>aceites</td>
-                                        <td>25/09/2021</td>
-                                        <td>25/09/2021</td>
-                                        <td>
-                                            <a href="" class="btn btn-danger"><i class="far fa-minus-square"></i></a>
-                                            <a href="" class="btn btn-primary"><i class="far fa-edit"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="" class="text-dark text-decoration-none">3</a></td>
-                                        <td>Producto Seleccionado</td>
-                                        <td>12.00</td>
-                                        <td>2</td>
-                                        <td>aceites</td>
-                                        <td>25/09/2021</td>
-                                        <td>25/09/2021</td>
-                                        <td>
-                                            <a href="" class="btn btn-danger"><i class="far fa-minus-square"></i></a>
-                                            <a href="" class="btn btn-primary"><i class="far fa-edit"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="" class="text-dark text-decoration-none">4</a></td>
-                                        <td>Producto Seleccionado</td>
-                                        <td>12.00</td>
-                                        <td>2</td>
-                                        <td>aceites</td>
-                                        <td>25/09/2021</td>
-                                        <td>25/09/2021</td>
-                                        <td>
-                                            <a href="" class="btn btn-danger"><i class="far fa-minus-square"></i></a>
-                                            <a href="" class="btn btn-primary"><i class="far fa-edit"></i></a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                </div>
-                <div class="col-lg-2">
-                    <!-- Boton de accion -->
-                    <div class="text-center border-left-danger">
-                        <input type="submit" value="Realizar Envio" class="btn btn-danger btn-rounded btn-lg">
-                    </div>
                 </div>
             </div>
         </div>
     </form>
 </section>
+
+
+<!-- MODAL de visualizacion-->
+
+<div class="modal fade" id="ubicacionModal" tabindex="-1" aria-labelledby="ubicacionModal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title py-2" id="exampleModalLabel">Ubicacion</h5>
+        <button type="button" class="btn btn-success" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">Ok</span>
+        </button>
+      </div>
+      <div class="modal-body text-center">
+        <table class="table">
+            <thead class="text-center text-light bg-warning">
+                <th>Pasillo</th>
+            </thead>
+            <tbody>
+                <td> 18</td>
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
