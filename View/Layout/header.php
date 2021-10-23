95e849f39dfe4d6e8ba344b0d5e0b776af94521a
Wilfredo Luna <61670708+LunaWilfredo@users.noreply.github.com>
2021-10-23 16:52:41 -0500

    Update header.php

--- View/Layout/header.php
+++ View/Layout/header.php
@@ -13,8 +13,8 @@ if(isset($_GET['cerrar']) && $_GET['cerrar'] == 'ok'){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Simulated of web multiplataform of supermarket">
+    <meta name="author" content="GroupOne">
 
     <title>Plaza Vea WebShop</title>
 
